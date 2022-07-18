f99fbdbcd14ab5763eff93c7d69ec12942cd018e
Divvy C <divvycr@gmail.com>
2022-07-18 15:57:20 +0100

    Introduce `CardView` and `HandPane`.

--- src/main/java/dvc/cabo/Card.java
+++ src/main/java/dvc/cabo/Card.java
@@ -8,48 +8,52 @@ public class Card {
     private boolean isFaceDown;
 
     public Card(int value, boolean isFaceDown) {
-        cardCount++;
-        cardID = cardCount * 508721;
+	cardCount++;
+	cardID = cardCount * 508721;
 
-        this.value = value;
-        this.isFaceDown = isFaceDown;
+	this.value = value;
+	this.isFaceDown = isFaceDown;
 
-        if (this.value == 11 || this.value == 12) {
-            this.action = "SWAP";
-        }
-        if (this.value == 9 || this.value == 10) {
-            this.action = "SPY";
-        }
-        if (this.value == 7 || this.value == 8) {
-            this.action = "PEEK";
-        }
+	if (this.value == 11 || this.value == 12) {
+	    this.action = "SWAP";
+	}
+	if (this.value == 9 || this.value == 10) {
+	    this.action = "SPY";
+	}
+	if (this.value == 7 || this.value == 8) {
+	    this.action = "PEEK";
+	}
     }
 
     public int getID() {
-        return cardID;
+	return cardID;
     }
 
     public void flipCard() {
-        this.isFaceDown = !this.isFaceDown;
+	this.isFaceDown = !this.isFaceDown;
+    }
+
+    public int getValue() {
+	return value;
     }
 
     public String getAction() {
-        return action;
+	return action;
     }
 
     public boolean isFaceDown() { return this.isFaceDown; }
 
     @Override
     public String toString() {
-        String s;
-        if (!isFaceDown) {
-            s = String.valueOf(this.value);
-            if (!action.equals("")) {
-                s = s + " (" + this.action + ")";
-            }
-        } else {
-            s = "???";
-        }
-        return s;
+	String s;
+	if (!isFaceDown) {
+	    s = String.valueOf(this.value);
+	    if (!action.equals("")) {
+		s = s + " (" + this.action + ")";
+	    }
+	} else {
+	    s = "???";
+	}
+	return s;
     }
 }
--- src/main/java/dvc/cabo/Main.java
+++ src/main/java/dvc/cabo/Main.java
@@ -1,29 +1,87 @@
 package dvc.cabo;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
+	GridPane root = new GridPane();
+	root.setAlignment(Pos.CENTER);
 
-        primaryStage.setTitle("Hello JavaFX!");
-        primaryStage.setScene(new Scene(root, 2560, 1440));
-        primaryStage.show();
+	HandPane hp = new HandPane();
+	hp.addCard(new Card(7, true));
+	hp.addCard(new Card(1, true));
+	hp.addCard(new Card(11, false));
+
+	root.add(hp, 0, 0);
+
+	primaryStage.setTitle("Hello JavaFX!");
+	primaryStage.setScene(new Scene(root, 2560, 1440));
+	primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+	launch(args);
 
 //        Logic logic = new Logic();
 //        logic.addPlayerByName("cabo.Player 1");
 //        logic.addPlayerByName("cabo.Player 2");
 //        logic.startGame();
     }
+}
+
+class CardView extends ImageView {
+
+    private Card card;
+    private Image seen;
+
+    public CardView(Card card) {
+	this.card = card;
+
+	setPreserveRatio(true);
+	setFitWidth(200);
+
+	seen = new Image(getClass().getResourceAsStream(card.getValue() + ".jpeg"));
+	setImage(seen);
+
+	setOnMouseClicked((MouseEvent e) -> {
+		System.out.println("Clicked!");
+		flip();
+	    });
+
+	if (card.isFaceDown()) setImage(new Image(getClass().getResourceAsStream("hidden.jpeg")));
+	else setImage(seen);
+    }
+
+    public void flip() {
+	card.flipCard();
+
+	if (card.isFaceDown()) setImage(new Image(getClass().getResourceAsStream("hidden.jpeg")));
+	else setImage(seen);
+    }
+
+}
+
+class HandPane extends HBox {
+
+    public HandPane() {
+	setStyle("-fx-border-color: #DDD; -fx-border-width: 4px; -fx-border-radius: 6px");
+	setSpacing(10);
+	setPadding(new Insets(15));
+    }
+
+    public void addCard(Card card) {
+	getChildren().add(new CardView(card));
+    }
+
 }
