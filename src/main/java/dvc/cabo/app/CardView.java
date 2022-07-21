fd61e0b62935fd2541c7e7e83325f8423f59f41c
Divvy C <divvycr@gmail.com>
2022-07-21 19:09:04 +0100

    Introduce `ImageLoader`, to pre-load all images for performance.

--- src/main/java/dvc/cabo/app/CardView.java
+++ src/main/java/dvc/cabo/app/CardView.java
@@ -1,10 +1,14 @@
 package dvc.cabo.app;
 
+import java.util.ArrayList;
+
 import dvc.cabo.logic.Card;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class CardView extends ImageView {
+
+    private static final ImageLoader imageLoader = new ImageLoader();
 
     private final Image HIDDEN;
     private final Image SEEN;
@@ -13,8 +17,8 @@ public class CardView extends ImageView {
 	setPreserveRatio(true);
 	setFitWidth(200);
 
-	SEEN = new Image(getClass().getResourceAsStream(card.getValue() + ".jpeg"));
-	HIDDEN = new Image(getClass().getResourceAsStream("hidden.jpeg"));
+	SEEN = imageLoader.getCardImageByValue(card.getValue());
+	HIDDEN = imageLoader.getHiddenCardImage();
 
 	if (card.isFaceDown()) setImage(HIDDEN);
 	else setImage(SEEN);
@@ -26,6 +30,25 @@ public class CardView extends ImageView {
 
     public void setHidden() {
 	setImage(HIDDEN);
+    }
+
+}
+
+class ImageLoader { // Pre-loading all images is KEY TO PERFORMANCE.
+
+    private static final ArrayList<Image> images = new ArrayList<>();
+
+    public ImageLoader() {
+	for (int i = 0; i < 14; i++) images.add(i, new Image(getClass().getResourceAsStream(i + ".jpeg")));
+	images.add(14, new Image(getClass().getResourceAsStream("hidden.jpeg")));
+    }
+
+    public Image getCardImageByValue(int cardValue) {
+	return images.get(cardValue);
+    }
+
+    public Image getHiddenCardImage() {
+	return images.get(14);
     }
 
 }
